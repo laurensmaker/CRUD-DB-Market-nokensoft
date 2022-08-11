9fe2e922a22b1fd393f4f929c15b94457ed04112
laurens <laurenkostan@gmial.com>
2022-08-11 15:48:29 +0900

    ubah asset menjadi secure_asset

--- resources/views/layouts/utama.blade.php
+++ resources/views/layouts/utama.blade.php
@@ -67,21 +67,21 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('/jquery/jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="{{ asset('jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ secure_asset('jquery-easing/jquery.easing.min.js') }}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+    <script src="{{ secure_asset('js/sb-admin-2.min.js') }}"></script>
 
     <!-- Page level plugins -->
-    <script src="{{ asset('chart.js/Chart.min.js') }}"></script>
+    <script src="{{ secure_asset('chart.js/Chart.min.js') }}"></script>
 
     <!-- Page level custom scripts -->
-    <script src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
-    <script src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
+    <script src="{{ secure_asset('js/demo/chart-area-demo.js') }}"></script>
+    <script src="{{ secure_asset('js/demo/chart-pie-demo.js') }}"></script>
 
 </body>
 
